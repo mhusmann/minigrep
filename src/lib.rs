3c30f0a11c8eee4db7a9d3b58e78f2fb8bca77c2
Michael Husmann <michaelhusmann@gmail.com>
2017-12-09 14:12:56 +0100

    better command line

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,50 @@
 use std::fs::File;
 use std::io::prelude::*;
 use std::error::Error;
-use std::env;
+extern crate clap;
+use clap::{Arg, App};
 
-const NOT_ENOUGH_ARGS: &str = "Not enough arguments!";
+fn get_commandline() -> (bool, String, String) {
+    let matches = App::new("commandline")
+        .version("0.0.1")
+        .author("Michael Husmann <michaelhusmann@gmail.com>")
+        .about("My own grep tool")
+        .arg(
+            Arg::with_name("search-pattern")
+                .help("the pattern you are looking for")
+                .required(true)
+                .index(1),
+        )
+        .arg(
+            Arg::with_name("file")
+                .help("the file to investigate")
+                .required(true)
+                .index(2),
+        )
+        .arg(Arg::with_name("i").short("i").long("ignore").help(
+            "case sensitive search",
+        ))
+        .get_matches();
+    let ignorecase = !matches.is_present("i");
+    let search = matches.value_of("search-pattern").unwrap();
+    let filename = matches.value_of("file").unwrap();
+
+    (ignorecase, search.to_string(), filename.to_string())
+}
 
 pub struct Config {
-    query: String,
-    filename: String,
+    pub query: String,
+    pub filename: String,
     pub case_sensitive: bool,
 }
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err(NOT_ENOUGH_ARGS);
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
-        println!("case_sensitive: {}", case_sensitive);
+    pub fn new() -> Result<Config, &'static str> {
+        let (case_sensitive, query, filename) = get_commandline();
         Ok(Config {
             query,
             filename,
             case_sensitive,
         })
-    }
-    pub fn query(&self) -> &str {
-        &self.query
-    }
-    pub fn filename(&self) -> &str {
-        &self.filename
     }
 }
 
@@ -50,14 +65,11 @@ pub fn run(config: &Config) -> Result<(), Box<Error>> {
     Ok(())
 }
 
-pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line)
-        }
-    }
-    results
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,16 @@
 extern crate minigrep;
-
-use std::env;
 use std::process;
 
 use minigrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config = &Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new().unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1)
     });
 
-    println!("Searching for {}", config.query());
-    println!("In file: {}", config.filename());
+    // println!("Searching for {}", config.query);
+    // println!("In file: {}\n", config.filename);
     if let Err(e) = minigrep::run(&config) {
         eprintln!("Application Error: {}", e);
         process::exit(1)
